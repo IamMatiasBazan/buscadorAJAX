02b2b8e1b41f126cabd74e5763d9e0f13a6a1c67
[DMatias <[Dbazanmatias2004@gmail.com>
2022-09-23 19:24:30 -0300

    Agregando el table en codigo PHP

---
+++ php/codigo.php
@@ -0,0 +1,13 @@
+<?php 
+    include("../database/abrir_conexion.php");
+
+    echo '
+        <table class="table table-hover">
+            <tr>
+                <th scope="col">NOMBRE</th>
+                <th scope="col">APELLIDO</th>
+                <th scope="col">DNI</th>
+                <th scope="col">EMAIL</th>
+            </tr>
+    ';
+?>
