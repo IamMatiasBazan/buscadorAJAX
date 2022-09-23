fb6bf4ebc29ba4b142e18b669d8e4415ed3828c4
[DMatias <[Dbazanmatias2004@gmail.com>
2022-09-23 19:44:31 -0300

    Agregando fila y columna en codigo php

--- php/codigo.php
+++ php/codigo.php
@@ -10,4 +10,19 @@
                 <th scope="col">EMAIL</th>
             </tr>
     ';
+
+    $resultados = mysqli_query($conexion, "SELECT * FROM $tabla1");
+    while($consulta = mysqli_fetch_array($resultados)) {
+        echo '
+            <tr>
+                <td>'.$consulta['id'].'</td>
+                <td>'.$consulta['nombre'].'</td>
+                <td>'.$consulta['apellido'].'</td>
+                <td>'.$consulta['dni'].'</td>
+                <td>'.$consulta['email'].'</td>
+            </tr>
+        ';
+    }
+
+    echo '</table>';
 ?>
