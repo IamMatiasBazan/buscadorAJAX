0f809a5f05fd2cc51d5b1cf947f5cc8a3fee917e
[DMatias <[Dbazanmatias2004@gmail.com>
2022-11-13 19:37:18 -0300

    Agregando comentarios

--- database/abrir_conexion.php
+++ database/abrir_conexion.php
@@ -1,10 +1,10 @@
 <?php 
-    $host="127.0.0.1";
-    $port=3306;
-    $socket="";
-    $user="root";
-    $password="";
-    $dbname="buscador";
+    $host="127.0.0.1"; /*Direccion de la IP*/
+    $port=3306; // Puerto 3306 para base de datos.
+    $socket=""; // Variables de entorno
+    $user="root"; // Usuario
+    $password=""; // Password
+    $dbname="buscador"; //Nombre de la base de datos
     
     $tabla1 = "clientes";
     $tabla2 = "empleados";
@@ -16,4 +16,6 @@
         echo "El sitio experimenta fallos";
         exit();
     }
+
+    /* Conexion simple a base de datos. Al trabajar con clases, es mejor usar PDO */
 ?>
